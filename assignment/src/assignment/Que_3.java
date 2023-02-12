8a7f1a32f2b6447ea0394b842870b7898607040b
Kaveri <Kaveri@DESKTOP-54V6S96>
2023-02-12 09:03:40 +0530

    My First Assignment

---
+++ assignment/src/assignment/Que_3.java
@@ -0,0 +1,34 @@
+package assignment;
+
+import java.util.Scanner;
+
+public class Que_3 {
+
+	public static void main(String[] args) {
+		
+		int k1,k2,k3;
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter 3 angles");
+			k1 = sc.nextInt();
+			k2 = sc.nextInt();
+			k3= sc.nextInt();
+		}
+		if((k1+k2+k3) == 180){
+		if(k1 == k2 && k2 == k3 && k1 == k3)
+		 {
+		System.out.println("Prize 3");
+		}
+		else if(k1 == 90 || k2 ==90 || k3 == 90) 
+		{
+		System.out.println("Prize 2");
+		}else 
+		{System.out.println("Prize 1");
+		}
+		}
+		else {
+		System.out.println("No prize");
+		}
+
+	}
+
+}
---
+++ assignment/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Kaveri
+ *
+ */
+module assignment {
+}
